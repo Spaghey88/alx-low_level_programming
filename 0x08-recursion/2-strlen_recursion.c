d4a422b0a2275d29310900a8d75e3c0bd142c0ec
spaghey88 <mensahshepherd@yahoo.com>
2022-11-02 21:01:54 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,9 +10,15 @@ int _strlen_recursion(char *s)
 {
 	int len = 0;
 
-	if (*s)
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	if (*s != '\0')
 	{
 		len++;
 		len += _strlen_recursion(s + 1);
+		return (len);
 	}
+	return (0);
 }
