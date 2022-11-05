fdf6e7107b7496f6fd6a04789c0f59d7b8d78bc5
spaghey88 <mensahshepherd@yahoo.com>
2022-11-04 20:40:14 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,9 @@
  * @argc: number arguments passed to the function
  * @argv: Argument vetor of pointers to strings
  *
- * Return: Always 0
+ * Return: Always 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc _attribute_((unused)), char *argv[])
 {
 	prinf("%s\n", argv[0]);
 	return (0);
